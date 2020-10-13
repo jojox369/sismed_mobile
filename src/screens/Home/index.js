82043362849023de1f4cf790afce4752561ffc3b
João Vitor <joaooviitorr@hotmail.com>
2020-10-13 09:56:42 -0300

    Alterado Icone android; Atualização na pagina home;

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,12 +3,11 @@ import {UserContext} from '../../contexts/UserContext';
 import {Container, HeaderArea, HeaderTitle} from './styles';
 import SismedIcon from '../../assets/icons/sismed.svg';
 export default () => {
-  const {state} = useContext(UserContext);
   return (
     <Container>
       <HeaderArea>
         <SismedIcon width="100%" height="160" />
-        <HeaderTitle>Seja Bem-vindo {state.nome} </HeaderTitle>
+        <HeaderTitle>Seja Bem-vindo(a)</HeaderTitle>
       </HeaderArea>
     </Container>
   );
